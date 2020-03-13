8c922fe69c97458a6d300a13f1808b15792f3748
castrogabe <gabewebdevelopment@gmail.com>
2020-03-12 19:01:44 -0700

    commit

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const expressValidator = require('express-validator');
+const fs = require('fs');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// db
+// ('MONGO_URI=mongodb://localhost/nodeapi');
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('DB Connected'));
 
-var app = express();
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`);
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// bring in routes
+const postRoutes = require('./routes/post');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+// apiDocs
+app.get('/', (req, res) => {
+  fs.readFile('docs/apiDocs.json', (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// middleware -
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
+app.use(expressValidator());
+app.use(cors());
+app.use('/', postRoutes);
+app.use('/', authRoutes);
+app.use('/', userRoutes);
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: 'Unauthorized!' });
+  }
 });
 
-module.exports = app;
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`A Node Js API is listening on port: ${port}`);
+});
